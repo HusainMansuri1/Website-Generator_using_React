7ce10b90fe691534b3bee71c01ecb2601806eec7
husain.m <husain.m>
2020-06-19 18:01:36 +0530

    state lifted -- errorprone

--- src/App.js
+++ src/App.js
@@ -7,15 +7,41 @@ import DyanamicContent from './containerComponents/dynamic-content';
 
 class App extends Component {
   componentsId = 1;
+  gfId = 1;
+
   state = {
-    components: [
-      { id: this.componentsId++, name: <Banner key='banner' />, show: true, btnText: 'Banner' },
-      { id: this.componentsId++, name: <People key='girlfriends' />, show: true, btnText: 'Friends' },
-      { id: this.componentsId++, name: <People key='more-girlfriends' />, show: false, btnText: 'More Friends' },
-      { id: this.componentsId++, name: <People key='more-more-girlfriends' />, show: false, btnText: 'More More Friends' },
+    components: [],
+    people: [
+      { id: this.gfId++, name: 'Nashra', age: '20'},
+      // { id: this.gfId++, name: 'Guess', age: '21' },
+      // { id: this.gfId++, name: 'Guess', age: '23' },
+      // { id: this.gfId++, name: 'Guess', age: '21' },
+      // { id: this.gfId++, name: 'Guess', age: '17' },
+      // { id: this.gfId++, name: 'Guess', age: '19' },
     ],
   };
 
+  // Dcom = [<Banner key='banner' />, <People pushPeopleState={this.state.people} key='girlfriends' />];
+  componentDidMount(){
+    this.setState({
+      components: [
+        { id: this.componentsId++, name: <Banner key='banner' />, show: false, btnText: 'Banner' },
+        { id: this.componentsId++, name: <People delete={this.deleteGfHandler} pushPeopleState={this.state.people} key='girlfriends' />, show: true, btnText: 'Friends' },
+        { id: this.componentsId++, name: <People pushPeopleState={this.state.people} key='more-girlfriends' />, show: false, btnText: 'More Friends' },
+        { id: this.componentsId++, name: <People pushPeopleState={this.state.people} key='more-more-girlfriends' />, show: false, btnText: 'More More Friends' },
+      ],
+    })
+  };
+
+  deleteGfHandler = (index, name) => {
+    // if(window.confirm(`Are you sure you want to delete  "${name}" from your life ?`)){
+      const people = [...this.state.people];
+      people.splice(index, 1);
+      this.setState({ people });
+    // }
+  };
+  
+  
   // state change
   componentsToggleHandler = (id) => {
     const components = [...this.state.components];
@@ -27,6 +53,7 @@ class App extends Component {
   getAddedComponents = () => (
     this.state.components.map((curr) => curr.show ? curr.name : null)
   );
+
 
   render() {
     return (
